275aa5b8fe316362d320d5dbbe6891b439b6cafc
4ertan <ertanosmanoglu13@gmail.com>
2023-03-22 17:25:56 +0300

    v1.2

---
+++ IdentityBlogApp.Web/IdentityBlogApp.Web/Extenisons/StartupExtenisons.cs
@@ -0,0 +1,21 @@
+﻿using IdentityBlogApp.Web.Models;
+
+namespace IdentityBlogApp.Web.Extenisons
+{
+    public static class StartupExtenisons
+    {
+        public static void AddIdentityWithExt(this IServiceCollection services)
+        {
+            services.AddIdentity<AppUser, AppRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnoprstuvwxyz1234567890_";
+                options.Password.RequiredLength = 6;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireDigit = true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+        }
+    }
+}
--- IdentityBlogApp.Web/IdentityBlogApp.Web/Program.cs
+++ IdentityBlogApp.Web/IdentityBlogApp.Web/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<AppDbContext>(options=>options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConn")));
-builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<AppDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,13 +26,13 @@ app.UseRouting();
 app.UseAuthorization();
 
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
 
 app.MapControllerRoute(
       name: "areas",
       pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
     );
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.Run();
